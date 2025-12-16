88b7dbb88eb94ae77f4bbd118281ca1ea3a4250c
Akash2001-sudo <akp18129@gmail.com>
2025-12-16 17:53:41 +0530

    logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import ExpenseForm from './components/ExpenseForm';
 import ExpenseList from './components/ExpenseList';
 import './App.css';
+import logo from './assets/logo.png';
 
 const App = () => {
   return (
     <>
-      <div className="bubbles-container">
-        <div className="bubble"></div>
-        <div className="bubble"></div>
-        <div className="bubble"></div>
-        <div className="bubble"></div>
-        <div className="bubble"></div>
-        <div className="bubble"></div>
-        <div className="bubble"></div>
-        <div className="bubble"></div>
-        <div className="bubble"></div>
-        <div className="bubble"></div>
-      </div>
+      <div className="background-container"></div>
       <div className="container">
         <nav className="navbar">
           <a href="#home" className="navbar-brand">
-            Spendly
+            <img src={logo} alt="Spendly Logo" className="logo" />
+            <span>Spendly</span>
           </a>
         </nav>
         <div className="row mt-4">
