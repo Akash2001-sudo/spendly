079bf092b0b8d1d07635db7002e421b9b94c1f8c
Akash2001-sudo <akp18129@gmail.com>
2025-12-16 18:00:20 +0530

    animation

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,15 @@ const App = () => {
         <nav className="navbar">
           <a href="#home" className="navbar-brand">
             <img src={logo} alt="Spendly Logo" className="logo" />
-            <span>Spendly</span>
+            <span className="spendly-text">
+              <span>S</span>
+              <span>p</span>
+              <span>e</span>
+              <span>n</span>
+              <span>d</span>
+              <span>l</span>
+              <span>y</span>
+            </span>
           </a>
         </nav>
         <div className="row mt-4">
